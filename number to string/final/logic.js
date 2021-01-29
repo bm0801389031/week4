d521b1b5a93bc23f747989274a075e5c51918399
Brian Monge <bm0801389031@gmail.com>
2021-01-28 20:57:55 -0700

    last commit complete file

---
+++ number to string/final/logic.js
@@ -0,0 +1,6 @@
+//set function numberToString with parameter num
+function numberToString(num){
+/*return num parameter with .toString() method; which turns a number
+to a string*/
+return num.toString();
+}
---
+++ number to string/test cases.js
@@ -0,0 +1,6 @@
+
+Test.assertEquals(numberToString(67), '67');
+Test.assertEquals(numberToString(79585), '79585');
+Test.assertNotEquals(numberToString(79585), 79585);
+Test.assertEquals(numberToString(1+2), '3');
+Test.assertEquals(numberToString(1-2), '-1');
